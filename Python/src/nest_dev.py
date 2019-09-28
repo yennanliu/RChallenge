838597b952fefb2f85231f3293081beb41c32b40
yennanliu <f339339@gmail.com>
2019-09-28 15:45:19 +0800

    add main json process logic func and output dev output

--- Python/src/nest_dev.py
+++ Python/src/nest_dev.py
@@ -17,13 +17,37 @@ def read_stdin_input():
 def read_file_input():
     pass
 
-def process_for_output():
-    pass 
+def append_not_listed(nlevels, data):
+    ''' Returns a list of dicts of not considered keys in nlevels '''
 
-def main():
-    pass 
+    not_listed = []
+    for key in data:
+        if key not in nlevels:
+            not_listed.append({key: data[key]})
 
+    return not_listed
+
+def process_for_output(input_data, json_keys):
+    """
+    parse CLI args, transform input data to json output  
+    """
+    output = {}
+    tmp = output
+    # main algorithm building the final data structure 
+    for data in input_data:
+        for i, key in enumerate(json_keys):
+            if i < len(json_keys) -1:
+                if data[key] not in tmp:
+                    tmp[data[key]] = {}
+                tmp = tmp[data[key]]
+            else:
+                tmp[data[key]] = append_not_listed(json_keys, data)
+        tmp = output
+    return output
 
 if __name__ == '__main__':
     json_data = read_stdin_input()
-    print (json_data)
+    #print (json_data)
+    json_keys = ['country' ,'city', 'currency']
+    output = process_for_output(json_data, json_keys)
+    print (json.dumps(output, indent=2))
