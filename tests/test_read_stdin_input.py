e356b294db58b14fdcb20689addd059dc9c5abff
yennanliu <f339339@gmail.com>
2019-09-28 23:49:25 +0800

    add test test_read_stdin_input.py

--- tests/test_read_stdin_input.py
+++ tests/test_read_stdin_input.py
@@ -2,24 +2,18 @@ import sys
 sys.path.append(".")
 import unittest
 from unittest.mock import patch
-from io import StringIO
 from nest.Nest import Json2NestedJson 
 
-
-# class test_read_stdin_input(unittest.TestCase):
-#     @patch('builtins.input', side_effect=[{"country": "US","city": "Boston","currency": "USD","amount": 100}])
-#     def run_test(self):
-#         json2nestedjons = Json2NestedJson()
-#         json_file = json2nestedjons.read_stdin_input()
-#         assert isinstance(json_file, list) == True 
-
-class test_read_stdin_input(unittest.TestCase):
-    @patch("sys.stdin", StringIO("country: US"))
-    def run_test(self):
-        json2nestedjons = Json2NestedJson()
-        json_file = json2nestedjons.read_stdin_input()
-        self.assertEqual(compare(), [{"country": "US"}]) 
+class TestReadStdinInput(unittest.TestCase):
+    """
+    mock Json2NestedJson.read_stdin_input method here 
+    and check if method return is in a needed form
+    """
+    def test_run(self):
+        with patch.object(Json2NestedJson, 'read_stdin_input', return_value=None) as mock_method:
+            json2nestedjon = Json2NestedJson()
+            json2nestedjon.read_stdin_input("country: US") 
+            mock_method.assert_called_once_with("country: US")
 
 if __name__ == '__main__':
-    #unittest.main()
-    pytest.main([__file__])
+    unittest.main()
