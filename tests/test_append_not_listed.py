3eb4b5591621fa609cf0c7978927c82fac1c4e5d
yennanliu <f339339@gmail.com>
2019-09-29 01:05:41 +0800

    add test test_append_not_listed.py

---
+++ tests/test_append_not_listed.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append(".")
+import unittest
+from unittest.mock import patch
+from nest.Nest import Json2NestedJson 
+
+class TestAppendNotListed(unittest.TestCase):
+    """
+    mock Json2NestedJson.append_not_listed method here 
+    and check if method return is in a needed form
+    """
+    def test_run(self):
+        json2nestedjon = Json2NestedJson()
+        data = {'amount': 100, 'city': 'Boston', 'country': 'US', 'currency': 'USD'}
+        json_keys = ["country","currency","city"]
+        tmp = json2nestedjon.append_not_listed(json_keys, data) 
+        assert tmp == [{'amount': 100}]
+
+if __name__ == '__main__':
+    unittest.main()
