34552de05873840593b7e348e4688366f0f61d11
yennanliu <f339339@gmail.com>
2019-09-28 17:56:51 +0800

    add test on read_file_input

---
+++ tests/test_read_file_input.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append(".")
+from nest.Nest import Json2NestedJson 
+
+def test_read_file_input():
+	json2nestedjons = Json2NestedJson()
+	json_file = json2nestedjons.read_file_input('data/input.json')
+	assert isinstance(json_file, list) == True 
+
+if __name__ == '__main__':
+    pytest.main([__file__])
