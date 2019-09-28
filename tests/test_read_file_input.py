29153efa6e8156886b3310ab5027e61cbb212add
yennanliu <f339339@gmail.com>
2019-09-28 23:52:35 +0800

    add test purposes to test scripts

--- tests/test_read_file_input.py
+++ tests/test_read_file_input.py
@@ -3,6 +3,9 @@ sys.path.append(".")
 from nest.Nest import Json2NestedJson 
 
 def test_read_file_input():
+	"""
+	test Json2NestedJson.read_file_input method 
+	"""
 	json2nestedjons = Json2NestedJson()
 	json_file = json2nestedjons.read_file_input('data/input.json')
 	assert isinstance(json_file, list) == True 
