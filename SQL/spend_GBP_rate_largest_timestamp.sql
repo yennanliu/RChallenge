9da04751495c4ea7554a6b1d48b30306f8082237
yennanliu <f339339@gmail.com>
2019-09-29 17:06:11 +0800

    fix CTE name and get aggregrated uuser GBP spend

--- SQL/spend_GBP_rate_largest_timestamp.sql
+++ SQL/spend_GBP_rate_largest_timestamp.sql
@@ -1,4 +1,4 @@
-WITH lastest_exchange_ts AS
+WITH largest_exchange_ts AS
   (SELECT from_currency,
           to_currency,
           max(ts) AS ts
@@ -6,31 +6,34 @@ WITH lastest_exchange_ts AS
    WHERE to_currency = 'GBP'
    GROUP BY from_currency,
             to_currency),
-     lastest_exchange AS
+     largest_timestamp_exchange AS
   (SELECT e.from_currency AS from_currency,
           e.rate AS rate
    FROM exchange_rates e
-   INNER JOIN lastest_exchange_ts l ON e.from_currency = l.from_currency
+   INNER JOIN largest_exchange_ts l ON e.from_currency = l.from_currency
    AND e.to_currency = l.to_currency
    AND e.ts = l.ts),
-     trans_GBP AS
+     trans_to_GBP AS
   (SELECT t.user_id AS user_id,
           t.ts AS ts,
           l.from_currency AS currency,
           t.amount*l.rate AS amount_GBP
    FROM transactions t
-   INNER JOIN lastest_exchange l ON l.from_currency = t.currency),
+   INNER JOIN largest_timestamp_exchange l ON l.from_currency = t.currency),
      trans_in_GBP AS
   (SELECT user_id,
           ts,
           currency,
           amount AS amount_gbp
    FROM transactions
-   WHERE currency = 'GBP' )
-SELECT *
-FROM trans_GBP
-UNION ALL
-SELECT *
-FROM trans_in_GBP
-ORDER BY user_id,
-         ts
+   WHERE currency = 'GBP' ),
+     trans_ AS
+  (SELECT *
+   FROM trans_to_GBP
+   UNION ALL SELECT *
+   FROM trans_in_GBP)
+SELECT user_id AS user_id,
+       SUM(amount_gbp) AS total_spent_gbp
+FROM trans_
+GROUP BY user_id
+ORDER BY user_id
