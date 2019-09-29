0ea04bc4f7f895b5fc7ff4c4c904af253125166d
yennanliu <f339339@gmail.com>
2019-09-29 17:00:51 +0800

    fix CTE name and get aggregrated uuser GBP spend

--- SQL/spend_GBP_rate_latest_transaction.sql
+++ SQL/spend_GBP_rate_latest_transaction.sql
@@ -5,20 +5,20 @@ WITH exchange_ts AS
           rate
    FROM exchange_rates
    WHERE to_currency = 'GBP'),
-     exchange_ts_ AS
+     exchange_ts_lag AS
   (SELECT *,
           lag(ts, -1, NULL) OVER (PARTITION BY from_currency,
                                                to_currency
                                   ORDER BY ts) AS ts_lag
    FROM exchange_rates
    WHERE to_currency = 'GBP' ),
-     trans_GBP AS
+     trans_to_GBP AS
   (SELECT t.user_id AS user_id,
           t.ts AS ts,
           t.currency AS currency,
           t.amount*e.rate AS amount_GBP
    FROM transactions t
-   INNER JOIN exchange_ts_ e ON e.from_currency = t.currency
+   INNER JOIN exchange_ts_lag e ON e.from_currency = t.currency
    AND (t.ts >= e.ts
         AND (e.ts_lag > t.ts
              OR e.ts_lag IS NULL))),
@@ -28,11 +28,14 @@ WITH exchange_ts AS
           currency,
           amount AS amount_gbp
    FROM transactions
-   WHERE currency = 'GBP' )
-SELECT *
-FROM trans_GBP
-UNION ALL
-SELECT *
-FROM trans_in_GBP
-ORDER BY user_id,
-         ts
+   WHERE currency = 'GBP' ),
+     trans_ AS
+  (SELECT *
+   FROM trans_to_GBP
+   UNION ALL SELECT *
+   FROM trans_in_GBP)
+SELECT user_id AS user_id,
+       SUM(amount_gbp) AS total_spent_gbp
+FROM trans_
+GROUP BY user_id
+ORDER BY user_id
