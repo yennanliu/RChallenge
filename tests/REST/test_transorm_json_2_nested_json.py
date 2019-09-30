bdfd28815e792c2b3630b2282be91d44710b877d
yennanliu <f339339@gmail.com>
2019-09-30 11:36:56 +0800

    add REST unit test test_transorm_json_2_nested_json.py

---
+++ tests/REST/test_transorm_json_2_nested_json.py
@@ -0,0 +1,20 @@
+import os, requests
+import pytest
+# REST flask app 
+import sys
+sys.path.append(".")
+from REST.app import * 
+
+def test_transorm_json_2_nested_json():
+    """
+    simulate the POST request with headers, data (CLI args), and API auth
+    test if API can work fine with http 201 status 
+    """
+    headers = {'Content-Type': 'application/json'}
+    data = '{"input_json":"data/input.json", "keys":["country", "city"]}'
+    auth = ('api_user', 'password')
+    response = requests.post('http://localhost:5000/REST/api/v1.0/nest', headers=headers, data=data, auth=auth)
+    assert response.status_code == 201
+
+if __name__ == '__main__':
+    pytest.main([__file__])
