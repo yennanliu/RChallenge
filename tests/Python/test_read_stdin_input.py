3302c9d69fef562621cd1b6171db2f8021f543a3
yennanliu <f339339@gmail.com>
2019-09-30 15:56:06 +0800

    rewrite Json2NestedJson method read_stdin_input for unit test test_read_stdin_input.py

--- tests/Python/test_read_stdin_input.py
+++ tests/Python/test_read_stdin_input.py
@@ -2,7 +2,23 @@ import sys
 sys.path.append(".")
 import unittest
 from unittest.mock import patch
+from io import StringIO 
+import json 
+# UDF 
 from nest.Nest import Json2NestedJson 
+
+class Json2NestedJson_inherit(Json2NestedJson):
+    """
+    Inherit Json2NestedJson class 
+    and re-write load stdin part in read_stdin_input method 
+    for below unit test
+    """
+    def read_stdin_input(self, mock_stdin):
+        output = ''
+        for line in mock_stdin:
+            output += line 
+        return json.loads(output)
+
 
 class TestReadStdinInput(unittest.TestCase):
     """
@@ -10,10 +26,10 @@ class TestReadStdinInput(unittest.TestCase):
     and check if method return is in needed form
     """
     def test_run(self):
-        with patch.object(Json2NestedJson, 'read_stdin_input', return_value=None) as mock_method:
-            json2nestedjon = Json2NestedJson()
-            json2nestedjon.read_stdin_input("country: US") 
-            mock_method.assert_called_once_with("country: US")
+        json2nestedjon = Json2NestedJson_inherit()
+        output = json2nestedjon.read_stdin_input('{"country":"UK","city": "London"}')
+        expetcted_output = {'country': 'UK', "city": "London"}  
+        assert output == expetcted_output
 
 if __name__ == '__main__':
     unittest.main()
