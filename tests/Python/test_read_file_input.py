9564bafaa9d2077a9448bb600d58e395ffa2ac72
yennanliu <f339339@gmail.com>
2019-09-30 14:57:56 +0800

    fix /test_read_file_input.py logic that compare expected output with output from read_file_input method

--- tests/Python/test_read_file_input.py
+++ tests/Python/test_read_file_input.py
@@ -6,9 +6,47 @@ def test_read_file_input():
     """
     test Json2NestedJson.read_file_input method 
     """
+    expected_output = [
+      {
+        "country": "US",
+        "city": "Boston",
+        "currency": "USD",
+        "amount": 100
+      },
+      {
+        "country": "FR",
+        "city": "Paris",
+        "currency": "EUR",
+        "amount": 20
+      },
+      {
+        "country": "FR",
+        "city": "Lyon",
+        "currency": "EUR",
+        "amount": 11.4
+      },
+      {
+        "country": "ES",
+        "city": "Madrid",
+        "currency": "EUR",
+        "amount": 8.9
+      },
+      {
+        "country": "UK",
+        "city": "London",
+        "currency": "GBP",
+        "amount": 12.2
+      },
+      {
+        "country": "UK",
+        "city": "London",
+        "currency": "FBP",
+        "amount": 10.9
+      }
+    ]
     json2nestedjons = Json2NestedJson()
     json_file = json2nestedjons.read_file_input('data/input.json')
-    assert isinstance(json_file, list) == True 
+    assert json_file == expected_output  
 
 if __name__ == '__main__':
     pytest.main([__file__])
